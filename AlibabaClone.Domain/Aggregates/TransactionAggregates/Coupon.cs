3812aae0f2c508a7094c04b82dc714562c1f49f8
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-18 13:45:21 +0330

    Add Transaction Aggregates: Transaction, Coupon

---
+++ AlibabaClone.Domain/Aggregates/TransactionAggregates/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.TransactionAggregates
+{
+    public class Coupon : Framework.Base.Entity<Guid>
+    {
+        public Coupon() {    }
+
+        public string Code { get; set; }
+        public int Percentage { get; set; }
+        public string? Descrption { get; set; }
+        public Guid? AccountId { get; set; }
+
+        public virtual AccountsAggregates.Account? Account { get; set; }
+    }
+}
---
+++ AlibabaClone.Domain/Aggregates/TransactionAggregates/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.TransactionAggregates
+{
+    public class Transaction : Framework.Base.Entity<Guid>
+    {
+        public Transaction() { }
+
+        public Guid TicketId { get; set; }
+        public decimal BaseAmount { get; set; }
+        public decimal FinalAmount { get; set; }
+        public string SerialNumber { get; set; }
+        public Guid? CouponId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual Ticket Ticket { get; set; }
+        public virtual Coupon? Coupon { get; set; }
+    }
+}
