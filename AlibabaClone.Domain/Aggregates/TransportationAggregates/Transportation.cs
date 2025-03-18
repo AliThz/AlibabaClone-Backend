9ab3824e917c55bb3d418dc012522d1adae0b840
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-18 13:45:46 +0330

    Add Transportation Aggregates: TicketStatus, Ticket, Transportation

---
+++ AlibabaClone.Domain/Aggregates/TransportationAggregates/Transportation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.TransportationAggregates
+{
+    public class Transportation : Framework.Base.Entity<Guid>
+    {
+        public Transportation() { }
+
+        public Guid FromLocationId { get; set; }
+        public Guid ToLocationId { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid VehicleId { get; set; }
+
+        public virtual LocationAggregates.Location FromLocation { get; set; }
+        public virtual LocationAggregates.Location ToLocation { get; set; }
+        public virtual CompanyAggregates.Company Company { get; set; }
+        public virtual VehicleAggregates.Vehicle Vehicle { get; set; }
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
