69a458e0e309ecec5db1daba229e993bf421835d
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-18 13:44:31 +0330

    Add Company Aggregates: Company

---
+++ AlibabaClone.Domain/Aggregates/CompanyAggregates/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.CompanyAggregates
+{
+    public class Company : Framework.Base.Entity<Guid>
+    {
+        public Company() { }
+
+        public required string Title { get; set; }
+        public virtual ICollection<TransportationAggregates.Transportation> Transportations { get; set; }
+    }
+}
