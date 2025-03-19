4249bc13f48265ad2eef72e665f794bf0ca24033
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-20 01:52:46 +0330

    Add Account Configuration

---
+++ AlibabaClone.Infrastructure/Configurations/AccountConfigurations/AccountConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountsAggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations.AccountConfigurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("Account");
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.PhoneNumber)
+                   .IsRequired()
+                   .HasMaxLength(11);
+
+            builder.Property(a => a.Password)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+
+            builder.Property(a => a.Email)
+                   .HasMaxLength(100);
+
+            builder.HasOne(a => a.Person)
+                   .WithMany(p => p.Accounts)
+                   .HasForeignKey(a => a.PersonId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
