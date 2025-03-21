866908e4757b88794efcdc220180cd47dd0b55a7
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 19:19:00 +0330

    Add Transportation Configuration

--- AlibabaClone.Infrastructure/Configurations/TransportationConfigurations/TransportationConfiguration.cs
+++ AlibabaClone.Infrastructure/Configurations/TransportationConfigurations/TransportationConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,76 @@ using System.Threading.Tasks;
 
 namespace AlibabaClone.Infrastructure.Configurations.TransportationConfigurations
 {
-    public class TransportationConfiguration
+    public class TransportationConfiguration : IEntityTypeConfiguration<Transportation>
     {
+        public void Configure(EntityTypeBuilder<Transportation> builder)
+        {
+            builder.ToTable("Transportation");
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.FromLocationId)
+                   .IsRequired();
+
+            builder.Property(t => t.ToLocationId)
+                   .IsRequired();
+
+            builder.Property(t => t.CompanyId)
+                   .IsRequired();
+
+            builder.Property(t => t.Vehicle)
+                   .IsRequired();
+
+            builder.Property(t => t.StartDateTime)
+                   .IsRequired();
+
+            builder.Property(t => t.EndDateTime)
+                   .IsRequired();
+
+            builder.Property(t => t.SerialNumber)
+                   .IsRequired()
+                   .HasMaxLength(63)
+                   .IsUnicode(false);
+
+            builder.Property(t => t.RemainingCapacity)
+                   .IsRequired();
+
+            builder.Property(t => t.BasePrice)
+                   .HasColumnType("decimal(18,2)")
+                   .IsRequired();
+
+            builder.Property(t => t.VIPPrice)
+                   .HasColumnType("decimal(18,2)");
+
+
+            builder.HasOne(t => t.FromLocation)
+                   .WithMany()
+                   .HasForeignKey(t => t.FromLocationId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.ToLocation)
+                   .WithMany()
+                   .HasForeignKey(t => t.ToLocationId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Buyer)
+                   .WithMany(a => a.BoughtTickets)
+                   .HasForeignKey(t => t.BuyerId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Traveler)
+                   .WithMany(p => p.TraveledTickets)
+                   .HasForeignKey(t => t.TravelerId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Companion)
+                   .WithMany()
+                   .HasForeignKey(t => t.CompanionId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.TicketStatus)
+                   .WithMany()
+                   .HasForeignKey(t => t.TicketStatusId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
