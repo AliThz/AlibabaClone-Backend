fe59977638d7f013e248931123f52b0380a342d4
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 21:29:02 +0330

    Add Seat Configuration

--- AlibabaClone.Infrastructure/Configurations/VehicleConfigurations/SeatConfiguration.cs
+++ AlibabaClone.Infrastructure/Configurations/VehicleConfigurations/SeatConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AlibabaClone.Domain.Aggregates.VehicleAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace AlibabaClone.Infrastructure.Configurations.VehicleConfigurations
 {
-    public class SeatConfiguration
+    public class SeatConfiguration : IEntityTypeConfiguration<Seat>
     {
+        public void Configure(EntityTypeBuilder<Seat> builder)
+        {
+            builder.ToTable("Seat");
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.VehicleId)
+                   .IsRequired();
+
+            builder.Property(s => s.Row)
+                   .IsRequired();
+
+            builder.Property(s => s.Column)
+                   .IsRequired();
+
+            builder.Property(s => s.IsVIP)
+                   .IsRequired();
+
+            builder.Property(s => s.IsAvailable)
+                   .IsRequired();
+
+
+            builder.HasOne(s => s.Vehicle)
+                   .WithMany(v => v.Seats)
+                   .HasForeignKey(s => s.VehicleId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
     }
 }
