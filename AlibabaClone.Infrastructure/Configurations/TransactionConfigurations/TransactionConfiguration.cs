ec470a48b0fa8896596cf58a4ac59899d4e1a0f2
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 18:39:18 +0330

    Add Transaction Configuration

---
+++ AlibabaClone.Infrastructure/Configurations/TransactionConfigurations/TransactionConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using AlibabaClone.Domain.Aggregates.TransactionAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations.TransactionConfigurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transaction");
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.TicketId)
+                   .IsRequired();
+
+            builder.Property(t => t.BaseAmount)
+                   .IsRequired()
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(t => t.FinalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(t => t.SerialNumber)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            builder.Property(t => t.CreatedAt)
+                   .IsRequired();
+
+            builder.Property(t => t.CouponId)
+                   .IsRequired(false);
+
+
+            builder.HasOne(t => t.Ticket)
+                   .WithMany()
+                   .HasForeignKey(t => t.TicketId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
