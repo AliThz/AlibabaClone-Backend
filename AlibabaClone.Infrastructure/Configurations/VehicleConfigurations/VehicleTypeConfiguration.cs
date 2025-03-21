babc556da3357d25372ab0491cf3261bd8b623a2
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 21:32:31 +0330

    Add VehicleType Configuration

--- AlibabaClone.Infrastructure/Configurations/VehicleConfigurations/VehicleTypeConfiguration.cs
+++ AlibabaClone.Infrastructure/Configurations/VehicleConfigurations/VehicleTypeConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AlibabaClone.Domain.Aggregates.VehicleAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace AlibabaClone.Infrastructure.Configurations.VehicleConfigurations
 {
-    public class VehicleTypeConfiguration
+    public class VehicleTypeConfiguration : IEntityTypeConfiguration<VehicleType>
     {
+        public void Configure(EntityTypeBuilder<VehicleType> builder)
+        {
+            builder.ToTable("VehicleType");
+            builder.HasKey(vt => vt.Id);
+            builder.Property(vt => vt.Id)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(vt => vt.Title)
+                   .IsRequired()
+                   .HasMaxLength(63)
+                   .IsUnicode(false);
+            builder.HasIndex(vt => vt.Id)
+                   .IsUnique();
+        }
     }
 }
