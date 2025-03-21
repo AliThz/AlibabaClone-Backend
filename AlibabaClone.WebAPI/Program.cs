76510becc90a79275d0b0afdda500d173755ddc9
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 21:48:14 +0330

    Add DbContext in WebAPI

--- AlibabaClone.WebAPI/Program.cs
+++ AlibabaClone.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+builder.Services.AddDbContext<AlibabaClone.Infrastructure.AlibabaCloneDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Development"));
+
+});
 
 app.UseHttpsRedirection();
 
