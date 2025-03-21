1dd64b6782d0ef10ee130de8fef81785d1cdcb4b
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 18:17:03 +0330

    Add City Configuration

---
+++ AlibabaClone.Infrastructure/Configurations/LocationConfigurations/CityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AlibabaClone.Domain.Aggregates.LocationAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations.LocationConfigurations
+{
+    public class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.ToTable("City");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Title)
+                   .IsRequired()
+                   .HasMaxLength(100);
+            builder.HasIndex(c => c.Title)
+                   .IsUnique();
+        }
+    }
+}
