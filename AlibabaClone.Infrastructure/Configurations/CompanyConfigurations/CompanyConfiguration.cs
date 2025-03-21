ccc67d266907bd32bbaec7f4993603580e9e5e70
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-21 18:14:49 +0330

    Add Company Configuration

---
+++ AlibabaClone.Infrastructure/Configurations/CompanyConfigurations/CompanyConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AlibabaClone.Domain.Aggregates.CompanyAggregates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations.CompanyConfigurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable("Company");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Title)
+                   .IsRequired()
+                   .HasMaxLength(127);
+            builder.HasIndex(c => c.Title)
+                   .IsUnique();
+        }
+    }
+}
