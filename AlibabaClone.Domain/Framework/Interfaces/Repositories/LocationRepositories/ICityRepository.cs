7feeb406c72d9068e6edaa5d1d8e4fff935904b1
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:31:46 +0330

    Add location related repositories

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/LocationRepositories/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.LocationAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.LocationRepositories
+{
+    public interface ICityRepository : IRepository<City, Guid>
+    {
+    }
+}
