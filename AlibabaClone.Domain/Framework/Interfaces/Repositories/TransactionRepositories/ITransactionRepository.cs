edcbcc02ea6a3e4fa5a9c0bd01837051fedea032
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 17:46:48 +0330

    FIx Transaction typed pased in this interface was from a wrong package

--- AlibabaClone.Domain/Framework/Interfaces/Repositories/TransactionRepositories/ITransactionRepository.cs
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransactionRepositories/ITransactionRepository.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using AlibabaClone.Domain.Aggregates.TransactionAggregates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Transactions;
 
 namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransactionRepositories
 {
