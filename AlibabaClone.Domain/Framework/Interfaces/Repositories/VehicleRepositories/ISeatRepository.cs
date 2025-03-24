6e7f9932a226d194c7cbab684ec49a29091979d8
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:38:53 +0330

    Add vehicle related repository interfaces

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/VehicleRepositories/ISeatRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.VehicleAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.VehicleRepositories
+{
+    public interface ISeatRepository : IRepository<Seat, Guid>
+    {
+    }
+}
