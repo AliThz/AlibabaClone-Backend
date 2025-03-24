a9732dbe679a16b6ebef30248528f25342a81a3d
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:20:13 +0330

    Add account related interface repositories

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/AccountRepositories/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountsAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories
+{
+    public interface IAccountRepository : IRepository<Account, Guid>
+    {
+    }
+}
