ff5169ba816a5bb6bb038487f9c58ce238be5eb4
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:33:34 +0330

    Add transaction related repositories

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransactionRepositories/ICouponRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.TransactionAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransactionRepositories
+{
+    public interface ICouponRepository : IRepository<Coupon, Guid>
+    {
+    }
+}
---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransactionRepositories/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransactionRepositories
+{
+    public interface ITransactionRepository : IRepository<Transaction, Guid>
+    {
+    }
+}
