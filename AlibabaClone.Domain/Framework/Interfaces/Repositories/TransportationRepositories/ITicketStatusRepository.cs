443941add53035f1252a03c211f1967d567994e8
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:35:59 +0330

    Add transportation related repositories

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransportationRepositories/ITicketStatusRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories
+{
+    public interface ITicketStatusRepository : IRepository<TicketStatus, short>
+    {
+    }
+}
