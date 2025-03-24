d3a81d89d86db8d190e0f02b2d9de8af6794049b
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-03-24 16:29:03 +0330

    Add company repository

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/CompanyRepositories/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlibabaClone.Domain.Aggregates.CompanyAggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.CompanyRepositories
+{
+    public interface ICompanyRepository : IRepository<Company, Guid>
+    {
+    }
+}
